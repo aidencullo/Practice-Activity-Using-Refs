ea76cc40b556c6be9b514797ea2db296511d09ee
Aiden Cullo <aidencullo@gmail.com>
2024-09-20 13:49:58 -0400

    Add a ref to the video element, and call its play() and pause() methods within the button's handleClick() function.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import { useState, useRef } from "react";
 
 export default function VideoPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
 
   function handleClick() {
     const nextIsPlaying = !isPlaying;
     setIsPlaying(nextIsPlaying);
+    if (videoRef.current) {
+      nextIsPlaying ? videoRef.current.play() : videoRef.current.pause();
+    }
   }
 
   return (
     <>
       <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
-      <video width="75%">
-        <source src="stock.mov" type="video/mp4" />
+      <video width="75%" ref={videoRef}>
+        <source src="/stock.mov" type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </>
   );
